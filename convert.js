65b8d3adec222e9049958b6b8fd68a5c861c17d3
eric-tikhonov <eric.tikhonov@tylertech.com>
2024-10-24 21:51:56 -0500

    separate assets

--- convert.js
+++ convert.js
@@ -5,7 +5,7 @@ const path = require("path"); // Ensure path module is included
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  let htmlContent = fs.readFileSync("index.html", "utf8");
+  let htmlContent = fs.readFileSync("assets/index.html", "utf8");
 
   // Remove headshot container and download button
   htmlContent = htmlContent.replace(
@@ -19,16 +19,16 @@ const path = require("path"); // Ensure path module is included
 
   await page.setContent(htmlContent, { waitUntil: "load" });
 
-  const cssContent = fs.readFileSync("styles.css", "utf8");
+  const cssContent = fs.readFileSync("assets/styles.css", "utf8");
   await page.addStyleTag({ content: cssContent });
 
   // Ensure scripts are evaluated correctly
-  const scriptPath = path.resolve(__dirname, "scripts.js");
+  const scriptPath = path.resolve(__dirname, "assets/scripts.js");
   await page.addScriptTag({ path: scriptPath });
 
   // Set the PDF options to scale content
   const pdfOptions = {
-    path: "resume.pdf",
+    path: "assets/resume.pdf",
     format: "A4",
     printBackground: true,
     scale: 0.9, // Adjust the scale to fit content better
